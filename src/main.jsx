f2e3fe4fa5019f380d5703f9eb66e1253475aadc
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-07-23 13:12:17 +0530

    update files

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import store from './redux/store.js'
-import { Provider } from 'react-redux'
-import { Toaster } from './components/ui/sonner.jsx'
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore } from 'redux-persist'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
+import { Toaster } from "./components/ui/sonner.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const persistor = persistStore(store);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -18,5 +18,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Toaster />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
