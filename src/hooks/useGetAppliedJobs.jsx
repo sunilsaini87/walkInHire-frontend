f4155950b42fe941c06856836c3adec481236d5e
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-07-23 13:09:31 +0530

    update hooks

--- src/hooks/useGetAppliedJobs.jsx
+++ src/hooks/useGetAppliedJobs.jsx
@@ -1,24 +1,26 @@
-import { setAllAppliedJobs } from '@/redux/applicationSlice';
-import axios from 'axios'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { setAllAppliedJobs } from "@/redux/applicationSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const useGetAppliedJobs = () => {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        const fetchAppliedJobs = async () => {
-            try {
-                axios.defaults.withCredentials = true;
-                const res = await axios.get('http://localhost:8000/api/v1/application/get');
-                if (res.data.success) {
-                    dispatch(setAllAppliedJobs(res.data.application))
-                }
-            } catch (error) {
-                console.log(error);
-            }
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchAppliedJobs = async () => {
+      try {
+        axios.defaults.withCredentials = true;
+        const res = await axios.get(
+          "http://localhost:8000/api/v1/application/get"
+        );
+        if (res.data.success) {
+          dispatch(setAllAppliedJobs(res.data.application));
         }
-        fetchAppliedJobs();
-    }, [])
-}
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchAppliedJobs();
+  }, []);
+};
 
-export default useGetAppliedJobs
+export default useGetAppliedJobs;
